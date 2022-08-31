51f4fac4d131c5274fe2784996491e66d66387a4
nishangsystems <nishangsystems@gmail.com>
2022-08-31 08:46:23 +0100

    add tel number

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
 
 <div class="w-100">
     <div class="w-100 bg-white d-flex justify-content-end py-2 flex-wrap">
-        <h6 class="fw-bolder text-dark px-2">Text or Call : 678324097 (<span class="fa fa-phone text-secondary" ></span> NELVINE) </h6> |
+        <h6 class="fw-bolder text-dark px-2">Text or Call : +237 6 95 78 44 24 (<span class="fa fa-phone text-secondary" ></span> NELVINE) </h6> |
         <h5 class="fw-bold px-2">
              <a href="#" class="mx-2"><span class="fab fa-facebook"> </span></a>
              <a href="#" class="mx-2"><span class="fab fa-instagram"> </span></a>
